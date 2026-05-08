ee6bbac88986edb4aa2e8b46c2329d6d355ef4ae
marrinda <maria.arrind@gmail.com>
2026-05-08 18:11:31 -0300

    feat: define AST types and implement AST destructors

--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.c
@@ -18,53 +18,196 @@ ModuleDestructor initializeAbstractSyntaxTreeModule() {
 	return _shutdownAbstractSyntaxTreeModule;
 }
 
+/* PRIVATE FUNCTIONS */
+
+static void _destroyBodyValue(void * value);
+static void _destroyDistanceValue(void * value);
+static void _destroyForceValue(void * value);
+static void _destroyImplicitForceValue(void * value);
+static void _destroySystemValue(void * value);
+
+static void _logAstDestructor(const char * functionName) {
+	logDebugging(_logger, "Executing destructor: %s", functionName);
+}
+
+static void _destroyBodyValue(void * value) {
+	destroyBody((Body *) value);
+}
+
+static void _destroyDistanceValue(void * value) {
+	destroyDistance((Distance *) value);
+}
+
+static void _destroyForceValue(void * value) {
+	destroyForce((Force *) value);
+}
+
+static void _destroyImplicitForceValue(void * value) {
+	destroyImplicitForce((ImplicitForce *) value);
+}
+
+static void _destroySystemValue(void * value) {
+	destroySystem((System *) value);
+}
+
 /* PUBLIC FUNCTIONS */
 
-void destroyConstant(Constant * constant) {
-	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
-	if (constant != NULL) {
-		free(constant);
+AstList * createAstList(void * value) {
+	AstList * list = calloc(1, sizeof(AstList));
+	if (list != NULL) {
+		list->value = value;
+	}
+	return list;
+}
+
+AstList * appendAstList(AstList * list, void * value) {
+	AstList * node = createAstList(value);
+	if (list == NULL) {
+		return node;
+	}
+	AstList * current = list;
+	while (current->next != NULL) {
+		current = current->next;
+	}
+	current->next = node;
+	return list;
+}
+
+void destroyAstList(AstList * list, AstNodeDestructor destroyValue) {
+	_logAstDestructor(__FUNCTION__);
+	while (list != NULL) {
+		AstList * next = list->next;
+		if (destroyValue != NULL) {
+			destroyValue(list->value);
+		}
+		free(list);
+		list = next;
 	}
 }
 
-void destroyExpression(Expression * expression) {
-	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
-	if (expression != NULL) {
-		switch (expression->type) {
-			case ADDITION:
-			case DIVISION:
-			case MULTIPLICATION:
-			case SUBTRACTION:
-				destroyExpression(expression->leftExpression);
-				destroyExpression(expression->rightExpression);
-				break;
-			case FACTOR:
-				destroyFactor(expression->factor);
-				break;
-		}
-		free(expression);
+void destroyBody(Body * body) {
+	_logAstDestructor(__FUNCTION__);
+	if (body != NULL) {
+		free(body->name);
+		body->name = NULL;
+		destroyMass(body->mass);
+		body->mass = NULL;
+		destroyAstList(body->forces, _destroyForceValue);
+		body->forces = NULL;
+		destroyImplicitForceList(body->implicitForces);
+		body->implicitForces = NULL;
+		free(body);
 	}
 }
 
-void destroyFactor(Factor * factor) {
-	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
-	if (factor != NULL) {
-		switch (factor->type) {
-			case CONSTANT:
-				destroyConstant(factor->constant);
-				break;
-			case EXPRESSION:
-				destroyExpression(factor->expression);
-				break;
-		}
-		free(factor);
+void destroyDirection(Direction * direction) {
+	_logAstDestructor(__FUNCTION__);
+	if (direction != NULL) {
+		free(direction);
+	}
+}
+
+void destroyDistance(Distance * distance) {
+	_logAstDestructor(__FUNCTION__);
+	if (distance != NULL) {
+		free(distance->fromBodyName);
+		distance->fromBodyName = NULL;
+		free(distance->toBodyName);
+		distance->toBodyName = NULL;
+		free(distance);
+	}
+}
+
+void destroyForce(Force * force) {
+	_logAstDestructor(__FUNCTION__);
+	if (force != NULL) {
+		free(force->name);
+		force->name = NULL;
+		destroyDirection(force->direction);
+		force->direction = NULL;
+		free(force);
+	}
+}
+
+void destroyFriction(Friction * friction) {
+	_logAstDestructor(__FUNCTION__);
+	if (friction != NULL) {
+		free(friction);
+	}
+}
+
+void destroyImplicitForce(ImplicitForce * implicitForce) {
+	_logAstDestructor(__FUNCTION__);
+	if (implicitForce != NULL) {
+		free(implicitForce);
+	}
+}
+
+void destroyImplicitForceList(ImplicitForceList * implicitForceList) {
+	_logAstDestructor(__FUNCTION__);
+	if (implicitForceList != NULL) {
+		destroyAstList(implicitForceList->forces, _destroyImplicitForceValue);
+		implicitForceList->forces = NULL;
+		free(implicitForceList);
+	}
+}
+
+void destroyMass(Mass * mass) {
+	_logAstDestructor(__FUNCTION__);
+	if (mass != NULL) {
+		free(mass);
 	}
 }
 
 void destroyProgram(Program * program) {
-	logDebugging(_logger, "Executing destructor: %s", __FUNCTION__);
+	_logAstDestructor(__FUNCTION__);
 	if (program != NULL) {
-		destroyExpression(program->expression);
+		destroyAstList(program->systems, _destroySystemValue);
+		program->systems = NULL;
 		free(program);
+	}
+}
+
+void destroyReferenceFrame(ReferenceFrame * referenceFrame) {
+	_logAstDestructor(__FUNCTION__);
+	if (referenceFrame != NULL) {
+		free(referenceFrame->bodyName);
+		referenceFrame->bodyName = NULL;
+		free(referenceFrame);
+	}
+}
+
+void destroySurface(Surface * surface) {
+	_logAstDestructor(__FUNCTION__);
+	if (surface != NULL) {
+		destroyFriction(surface->friction);
+		surface->friction = NULL;
+		free(surface);
+	}
+}
+
+void destroySystem(System * system) {
+	_logAstDestructor(__FUNCTION__);
+	if (system != NULL) {
+		free(system->name);
+		system->name = NULL;
+		destroyUnits(system->units);
+		system->units = NULL;
+		destroySurface(system->surface);
+		system->surface = NULL;
+		destroyAstList(system->bodies, _destroyBodyValue);
+		system->bodies = NULL;
+		destroyReferenceFrame(system->referenceFrame);
+		system->referenceFrame = NULL;
+		destroyAstList(system->distances, _destroyDistanceValue);
+		system->distances = NULL;
+		free(system);
+	}
+}
+
+void destroyUnits(Units * units) {
+	_logAstDestructor(__FUNCTION__);
+	if (units != NULL) {
+		free(units);
 	}
 }
--- src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
+++ src/main/c/frontend/syntactic-analysis/AbstractSyntaxTree.h
@@ -3,76 +3,220 @@
 
 #include "../../support/logging/Logger.h"
 #include "../../support/type/ModuleDestructor.h"
+#include <stdbool.h>
 #include <stdlib.h>
 
 /** Initialize module's internal state. */
 ModuleDestructor initializeAbstractSyntaxTreeModule();
 
 /**
- * This type definitions allows self-referencing types (e.g., an expression
- * that is made of another expressions, such as talking about you in 3rd
- * person, but without the madness).
+ * Generic AST list used by nodes that can contain repeated children.
  */
 
-typedef enum ExpressionType ExpressionType;
-typedef enum FactorType FactorType;
+typedef struct AstList AstList;
+typedef void (*AstNodeDestructor)(void *);
 
-typedef struct Constant Constant;
-typedef struct Expression Expression;
-typedef struct Factor Factor;
+/**
+ * Gravitas AST node declarations.
+ */
+
+typedef enum BodyShape BodyShape;
+typedef enum DirectionType DirectionType;
+typedef enum DistanceType DistanceType;
+typedef enum ForceUnit ForceUnit;
+typedef enum ImplicitForceType ImplicitForceType;
+typedef enum MassUnit MassUnit;
+typedef enum ReferenceFrameType ReferenceFrameType;
+typedef enum SurfaceType SurfaceType;
+typedef enum DistanceUnit DistanceUnit;
+
+typedef struct Body Body;
+typedef struct Direction Direction;
+typedef struct Distance Distance;
+typedef struct Force Force;
+typedef struct Friction Friction;
+typedef struct ImplicitForce ImplicitForce;
+typedef struct ImplicitForceList ImplicitForceList;
+typedef struct Mass Mass;
 typedef struct Program Program;
+typedef struct ReferenceFrame ReferenceFrame;
+typedef struct Surface Surface;
+typedef struct System System;
+typedef struct Units Units;
 
 /**
  * Node types for the Abstract Syntax Tree (AST).
  */
 
-enum ExpressionType {
-	ADDITION,
-	DIVISION,
-	FACTOR,
-	MULTIPLICATION,
-	SUBTRACTION
+enum BodyShape {
+	BODY_SHAPE_DEFAULT,
+	BODY_SHAPE_BLOCK,
+	BODY_SHAPE_SPHERE
 };
 
-enum FactorType {
-	CONSTANT,
-	EXPRESSION
+enum DirectionType {
+	DIRECTION_TYPE_ABSOLUTE_ANGLE,
+	DIRECTION_TYPE_PARALLEL_TO_SURFACE
 };
 
-struct Constant {
-	int value;
+enum DistanceType {
+	DISTANCE_TYPE_POLAR,
+	DISTANCE_TYPE_CARTESIAN
 };
 
-struct Factor {
+enum ForceUnit {
+	FORCE_UNIT_DEFAULT,
+	FORCE_UNIT_NEWTON
+};
+
+enum ImplicitForceType {
+	IMPLICIT_FORCE_WEIGHT,
+	IMPLICIT_FORCE_NORMAL,
+	IMPLICIT_FORCE_FRICTION
+};
+
+enum MassUnit {
+	MASS_UNIT_DEFAULT,
+	MASS_UNIT_KG
+};
+
+enum ReferenceFrameType {
+	REFERENCE_FRAME_ALIGNED_WITH_SURFACE,
+	REFERENCE_FRAME_ABSOLUTE
+};
+
+enum SurfaceType {
+	SURFACE_TYPE_HORIZONTAL,
+	SURFACE_TYPE_INCLINE
+};
+
+enum DistanceUnit {
+	DISTANCE_UNIT_DEFAULT,
+	DISTANCE_UNIT_METER
+};
+
+struct AstList {
+	void * value;
+	AstList * next;
+};
+
+struct Units {
+	bool hasMassUnit;
+	bool hasForceUnit;
+	bool hasDistanceUnit;
+	MassUnit massUnit;
+	ForceUnit forceUnit;
+	DistanceUnit distanceUnit;
+};
+
+struct Friction {
+	double staticCoefficient;
+	double kineticCoefficient;
+};
+
+struct Surface {
+	SurfaceType type;
+	bool hasAngle;
+	double angle;
+	Friction * friction;
+};
+
+struct Mass {
+	double value;
+	MassUnit unit;
+};
+
+struct Direction {
+	DirectionType type;
+	double angle;
+};
+
+struct Force {
+	char * name;
+	double magnitude;
+	ForceUnit unit;
+	Direction * direction;
+};
+
+struct ImplicitForce {
+	ImplicitForceType type;
+};
+
+struct ImplicitForceList {
+	AstList * forces;
+};
+
+struct Body {
+	char * name;
+	BodyShape shape;
+	Mass * mass;
+	AstList * forces;
+	ImplicitForceList * implicitForces;
+};
+
+struct ReferenceFrame {
+	ReferenceFrameType type;
+	char * bodyName;
+};
+
+struct Distance {
+	DistanceType type;
+	char * fromBodyName;
+	char * toBodyName;
 	union {
-		Constant * constant;
-		Expression * expression;
-	};
-	FactorType type;
-};
-
-struct Expression {
-	union {
-		Factor * factor;
 		struct {
-			Expression * leftExpression;
-			Expression * rightExpression;
-		};
+			double magnitude;
+			DistanceUnit magnitudeUnit;
+			double angle;
+		} polar;
+		struct {
+			double x;
+			DistanceUnit xUnit;
+			double y;
+			DistanceUnit yUnit;
+		} cartesian;
 	};
-	ExpressionType type;
+};
+
+struct System {
+	char * name;
+	Units * units;
+	bool hasGravity;
+	double gravity;
+	Surface * surface;
+	AstList * bodies;
+	ReferenceFrame * referenceFrame;
+	AstList * distances;
 };
 
 struct Program {
-	Expression * expression;
+	AstList * systems;
 };
 
 /**
- * Node recursive super-duper-trambolik-destructors.
+ * Generic list helpers.
  */
 
-void destroyConstant(Constant * constant);
-void destroyExpression(Expression * expression);
-void destroyFactor(Factor * factor);
+AstList * createAstList(void * value);
+AstList * appendAstList(AstList * list, void * value);
+void destroyAstList(AstList * list, AstNodeDestructor destroyValue);
+
+/**
+ * Node recursive destructors.
+ */
+
+void destroyBody(Body * body);
+void destroyDirection(Direction * direction);
+void destroyDistance(Distance * distance);
+void destroyForce(Force * force);
+void destroyFriction(Friction * friction);
+void destroyImplicitForce(ImplicitForce * implicitForce);
+void destroyImplicitForceList(ImplicitForceList * implicitForceList);
+void destroyMass(Mass * mass);
 void destroyProgram(Program * program);
+void destroyReferenceFrame(ReferenceFrame * referenceFrame);
+void destroySurface(Surface * surface);
+void destroySystem(System * system);
+void destroyUnits(Units * units);
 
 #endif
